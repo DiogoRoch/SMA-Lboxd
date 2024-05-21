995496a3512cc85539460cc1d700056c71858389
portmannh <168282886+portmannh@users.noreply.github.com>
2024-05-21 12:38:25 +0200

    Update bilouvain_module.py

--- bilouvain_module.py
+++ bilouvain_module.py
@@ -1,9 +1,25 @@
+"""
+This module contains our implementation of the bilouvain algorithm from the following paper:
+C. Zhou, L. Feng, and Q. Zhao, “A novel community detection method in bipartite networks,” 
+Physica A: Statistical Mechanics and its Applications, vol. 492, pp. 1679–1693, Feb. 2018, 
+doi: 10.1016/j.physa.2017.11.089.
+
+Group: Sema Aydin, Hannah Portmann, Diogo Rocha
+Implementation: Hannah Portmann
+"""
+
 import networkx as nx
 import numpy as np
 
 def set_weights_to_one(G):
     """
-    In the beginning, set all weights of the edges in the graph to one.
+    Set all weights of the edges in the graph to one in the beginning of the algorithm
+
+    Parameters:
+    G: graph
+
+    Returns:
+    Nothing, outcome is that all edges of graph G now have an edge weight of 1
     """
     for u, v in G.edges():
         G[u][v]['weight'] = 1
@@ -11,8 +27,13 @@ def set_weights_to_one(G):
 
 def set_group_order(G):
     """
-    Set the order of the groups because the algorithm needs to start with the larger group (here called group red).
-    G needs to be a bipartite graph with the attribute node_type with values 'top' and 'bottom' for the two node groups.
+    Set the order of the groups because the algorithm needs to start with the larger group (here called group red)
+
+    Parameters:
+    G: bipartite graph with the attribute node_type with values 'top' and 'bottom' for the two node groups
+
+    Returns:
+    groups: dictionary with keys 'red' and 'blue' and the corresponding group of nodes as values
     """
     top_nodes = {n for n, d in G.nodes(data=True) if d["node_type"] == "top"}
     bottom_nodes = set(G) - top_nodes
@@ -33,6 +54,14 @@ def set_group_order(G):
 def find_neighbor_communities(G, node, community_assignments):
     """
     Given a node, find the neighboring communities in graph G (depending on the current community_assignments).
+
+    Parameters:
+    G: bipartite graph
+    node: node that we want to find the neighboring communities of
+    community_assignments: dict which contains current communities
+
+    Returns:
+    neighboring_communities: set which contains all communities that are directly connected to node
     """
     neighboring_communities = set()
 
@@ -57,7 +86,16 @@ def find_neighbor_communities(G, node, community_assignments):
 
 def bipartite_modularity_gain(G, node_i, community, group):
     """
-    Compute the modularity gain of moving node_i to community. Group is the group of node_i.
+    Compute the modularity gain of moving node_i to community. 
+
+    Parameters:
+    G: bipartite graph
+    node_i: node that we move into community
+    community: community that node is moved into
+    group: node group of node_i
+
+    Returns:
+    delta_q: bipartite modularity gain of moving node_i into community
     """
     comm_other = [node for node in community if
                   node not in group]  # only take into account nodes in community from other node group
@@ -82,6 +120,14 @@ def bipartite_modularity_gain(G, node_i, community, group):
 def move_node(node, add_community, community_assignments):
     """
     Move a node to a new community and update the community assignments.
+
+    Parameters:
+    node: node that should be moved
+    add_community: community to which node will be added
+    community_assignment: dict containing current communities
+
+    Returns:
+    community_assignments: dict which contains the changed communities, where node is now in add_community
     """
     add_community_key, remove_community_key = None, None
 
@@ -106,6 +152,14 @@ def move_node(node, add_community, community_assignments):
 def assignment(G, group, community_assignments):
     """
     Assignment step: assign nodes of current node group to communities.
+
+    Parameters:
+    G: bipartite graph
+    group: list of nodes in current group, nodes of this group are assigned to communities
+    community_assignments: dict which contains the current communities as values
+
+    Returns:
+    new_assignments: new dict which contains the new communities
     """
     new_assignments = community_assignments.copy()
 
@@ -146,6 +200,18 @@ def assignment(G, group, community_assignments):
 def aggregation(group, group_key, community_assignments, G, iteration):
     """
     Aggregation step: aggregate nodes of current node group to hypernodes.
+
+    Parameters:
+    group: list of nodes belonging to current group, from which the nodes will be aggregated to hypernodes
+    group_key: key of current group (red or blue)
+    community_assignments: dict containing current communities
+    G: biparite graph
+    iteration: integer that indicates current iteration, used to name hypernodes
+
+    Returns:
+    H: new hypergraph, where nodes from group which are in the same community are aggregated to hypernodes
+    hypernodes: dict, which contains the names of hypernodes as keys and the values are the original nodes that were aggregate to that hypernode
+    new_assignments: dict containing the new community assignments, where the nodes from group are replaced by hypernodes
     """
     H = nx.Graph()  # create new hypergraph
     hypernodes = dict()  # create dict for storing hypernodes
@@ -195,6 +261,17 @@ def aggregation(group, group_key, community_assignments, G, iteration):
 
 
 def add_rows_and_columns(matrix, i, j):
+    """
+    Adds rows i and j and columns i and j of matrix (m x m) together.
+
+    Parameters:
+    matrix: original similarity matrix
+    i: index of community 1 that should be combined with community 2
+    j: index of community 2 that should be combined with community 1
+
+    Returns:
+    matrix: new matrix (m-1 x m-1), where rows i and j and columns i and j were added together
+    """
     matrix[i] = matrix[i] + matrix[j]
     matrix = np.delete(matrix, j, axis=0)
     matrix[:, i] = matrix[:, i] + matrix[:, j]
@@ -203,6 +280,16 @@ def add_rows_and_columns(matrix, i, j):
 
 
 def find_max_indices_and_update(S):
+    """
+    Finds the index of the maximum value in the matrix S and if the value is positive, combines the rows and columns of those indices.
+
+    Parameters: 
+    S: similarity matrix, element S_i_j represents modularity gain of combining communities i and j
+
+    Returns:
+    max_indices: the indices of the maximum value in matrix S, if this value is positive, e.g. i and j
+    S_new: matrix S, where the rows i and j and columns i and j were added together, representing the combination of those communities
+    """
     max_indices = np.unravel_index(np.argmax(S), S.shape)
     if S[max_indices] > 0:
         S_new = add_rows_and_columns(S, max_indices[0], max_indices[1])
@@ -212,6 +299,18 @@ def find_max_indices_and_update(S):
 
 
 def combine_communities(community_order, max_indices, community_assignments):
+    """ 
+    Now also combining two communities in the community assignments.
+
+    Parameters:
+    community_order: ordered list of communities, to identify communities by index
+    max_indices: indices of two communities that should be combined
+    community_assignments: dict with current communities
+
+    Returns:
+    community_assignments: dict with new communities, where the two communities are combined
+    community_order: updated ordered list of communities, where the two communities are combined
+    """
     comm_1 = community_order[max_indices[0]]
     comm_2 = community_order[max_indices[1]]
 
@@ -224,6 +323,19 @@ def combine_communities(community_order, max_indices, community_assignments):
 
 
 def recursive_update(G, m, community_assignments, S, community_order):
+    """
+    Updates community_assignments and S recursively, until no combination of communities leads to modularity gain anymore.
+
+    Parameters:
+    G: bipartite Graph, currently containg hypernodes created by previous assignment and aggregation steps
+    m: number of edges in original graph (corresponds to sum of edge weight in current graph G)
+    community_assignments: dict with current communities
+    S: similarity matrix, element S_i_j represents modularity gain of combining communities i and j
+    community_order: list of sorted community_ids
+
+    Returns:
+    community_assignments: final communities of hypernodes, where no combination of communities would increase modularity anymore
+    """
     max_indices, S_new = find_max_indices_and_update(S)
 
     if max_indices is not None and S_new is not None:
@@ -236,13 +348,27 @@ def recursive_update(G, m, community_assignments, S, community_order):
 
 
 def clustering(G, m, community_assignments):
+    """
+    Clustering algorithm implementation in matrix form. Communities are combined, if their combination increases bipartite modularity. 
+    
+    Parameters:
+    G: bipartite Graph, currently containg hypernodes created by previous assignment and aggregation steps
+    m: number of edges in original graph (corresponds to sum of edge weight in current graph G)
+    community_assignments: dict with current communities
+
+    Returns:
+    Result of recursive_update function
+    The following new parameters are given to this function:
+    S: similarity matrix, element S_i_j represents modularity gain of combining communities i and j
+    community_order: list of sorted community_ids
+    """
     community_order = sorted([community_id for community_id in community_assignments.keys()])
     sorted_nodes_blue = [node for community_id in community_order for node in community_assignments[community_id] if
                          node.startswith('b')]
     sorted_nodes_red = [node for community_id in community_order for node in community_assignments[community_id] if
                         node.startswith('r')]
     sorted_nodes = sorted_nodes_blue + sorted_nodes_red
-    # n = len(sorted_nodes)
+    
     comm_len = len(community_order)
 
     adj_matrix = nx.adjacency_matrix(G, nodelist=sorted_nodes)
@@ -261,6 +387,13 @@ def clustering(G, m, community_assignments):
 def unpack_hypernodes(node, all_hypernodes):
     """
     Unpack hypernode recursively to find original nodes which were aggregated to hypernode.
+
+    Parameters:
+    node: hypernode, which has to be unpacked
+    all_hypernodes: dict which contains for each hypernode a list of nodes, which were aggregated to that hypernode
+
+    Returns: 
+    List of original nodes contained in a hypernode
     """
     if node in all_hypernodes:
         return [unpack_hypernodes(sub_node, all_hypernodes) for sub_node in all_hypernodes[node]]
@@ -271,6 +404,12 @@ def unpack_hypernodes(node, all_hypernodes):
 def flatten(nested_list):
     """
     Flatten nested list which was created by unpack_hypernodes function.
+
+    Parameters:
+    nested_list: nested list that originates when hypernodes are unpacked
+
+    Returns: 
+    1 dimensional list containing nodes from nested_list
     """
     if len(nested_list) == 0:
         return nested_list
@@ -282,6 +421,13 @@ def flatten(nested_list):
 def final_communities(community_assignments, all_hypernodes):
     """
     Find communities of original nodes by unpacking all hypernodes.
+
+    Parameters:
+    community_assignments: dict with current community_assignments, containing hypernodes
+    all_hypernodes: dict containing information about which nodes were aggregated to form a hypernode
+
+    Returns:
+    final_community_ass: final assignments of communities, containing only the original nodes of the graph given to the algorithm
     """
     final_community_ass = dict()
 
@@ -301,8 +447,13 @@ def final_communities(community_assignments, all_hypernodes):
 
 def bilouvain(Graph):
     """
-    Find communities in a bipartite graph according to bilouvain algorithm by Zhou et al. (2018).
-    G has to be a bipartite graph with node type as an attribute.
+    Main function to find communities in a bipartite graph according to bilouvain algorithm by Zhou et al. (2018).
+    
+    Parameters:
+    Graph: a bipartite graph with node attributes "node_type" set to the values "top" and "bottom"
+
+    Returns: 
+    final_community_assignments: dictionary with the final communities as values, the key does not have any meaning anymore
     """
 
     G = Graph.copy()
